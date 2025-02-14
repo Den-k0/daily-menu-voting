71b431123e322b65b4091a62f6e63d37561b7f0f
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-14 18:04:15 +0200

    Implement votes serializers, views, urls

---
+++ votes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from votes.views import (
+    VoteViewSet,
+)
+
+router = DefaultRouter()
+router.register('votes', VoteViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "votes"
