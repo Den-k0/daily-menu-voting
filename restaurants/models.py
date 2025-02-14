f91c43c8c8dac4619778b4ff31540f74218822d5
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-14 13:01:42 +0200

    Implement restaurants models & add to admin

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Dishes"
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menus")
+    date = models.DateField(default=timezone.now, unique=True)
+    dishes = models.ManyToManyField(Dish, related_name="menus")
+
+    def __str__(self):
+        return f"Menu for {self.date} at {self.restaurant.name}"
