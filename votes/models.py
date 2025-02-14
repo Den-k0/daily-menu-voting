a00b7fea2566a200a19f6d311e2752e92f05a1a2
Den_k0 <denys.kotovskiy@gmail.com>
2025-02-14 13:02:04 +0200

    Implement votes models & add to admin

--- votes/models.py
+++ votes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from restaurants.models import Menu
+from users.models import Employee
+
+
+class Vote(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="votes")
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name="votes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("employee", "menu")
+
+    def __str__(self):
+        return f"{self.employee.email} voted for {self.menu.restaurant.name} on {self.menu.date}"
